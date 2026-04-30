3d628ad384bc74d9fb273f0d8a3aaac46b081bd6
Gabriel Santos <gs5560752@gmail.com>
2026-04-30 15:30:46 -0300

    updt: ajuste na segurança da API

--- src/main/java/br/edu/ufrn/tanninsdb/auth/config/SecurityConfig.java
+++ src/main/java/br/edu/ufrn/tanninsdb/auth/config/SecurityConfig.java
@@ -49,9 +49,10 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*")); // Ajustar para produção
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
+        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
